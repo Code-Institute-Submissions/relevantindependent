ed79dcdd774aa950facf2e3e1b86736bb30b8458
Alex Miller <alexander.dean.miller@gmail.com>
2017-01-07 21:59:18 +0000

    new updates

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-angular.module('RelevantIndependent', ['ngRoute', 'RouteControllers']);
+angular.module("RelevantIndependent", ["ngRoute", "RouteControllers"]);
  
-angular.module('RelevantIndependent').config(function($locationProvider, $routeProvider) {
+angular.module("RelevantIndependent").config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
  
-    $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeController'
+    $routeProvider.when("/", {
+        templateUrl: "templates/home.html",
+        controller: "HomeController"
     })
-    .when('/accounts/register', {
-        templateUrl: 'templates/register.html',
-        controller: 'RegisterController'
+    .when("/accounts/register", {
+        templateUrl: "templates/register.html",
+        controller: "RegisterController"
     });
 });
